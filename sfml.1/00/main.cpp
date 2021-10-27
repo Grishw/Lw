0401ca464c25e0b0455d4738023a8801cc6edd57
Grishw <57558364+Grishw@users.noreply.github.com>
2021-10-27 19:10:20 +0300

    add lw2 part1

---
+++ sfml.1/00/main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	cout << "enter two numbers:" << endl;
+	cin >> a >> b;
+    cout << "sum: " << (a + b) << endl;
+}
